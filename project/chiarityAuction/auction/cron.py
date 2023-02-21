5b8b7384b25b2375efe2729bb5a217513c9799d2
Giuseppe <88315256+JIMMI74@users.noreply.github.com>
2023-02-21 20:49:29 +0100

    Update cron.py

--- project/chiarityAuction/auction/cron.py
+++ project/chiarityAuction/auction/cron.py
@@ -5,12 +5,5 @@ def newbatch():
     print('lancio batch')
     result = batch()
     print('fine batch', result)
-    # if result == 'ok':
-    #     state = AuctionListing.objects.filter(active=True)
-    #     print('state', State)
-    #     for obj in state:
-    #         obj.active = False
-    #         obj.save()
-    #         print('fine batch', obj)
-
+   
 
